70996a09f88fdfaa6b06fd958b2ee6e444f4d17d
”NTkafep” <kaykefpereira@gmail.com>
2026-05-28 19:38:56 -0300

    Classe itemPedido: Implementação de validações, Excemptions. Melhorias no construtor

--- ItemPedido.java
+++ ItemPedido.java
@@ -4,18 +4,36 @@ public class ItemPedido {
     private int quantidade;
 
     public ItemPedido(Produto produto, int quantidade) {
+        validarQuantidade(quantidade);
+        validarProduto(produto);
         this.produto = produto;
         this.quantidade = quantidade;
     }
-    double getSubtotal() {
+    public void setQuantidade(int quantidade){
+        validarQuantidade(quantidade);
+        this.quantidade = quantidade;
+    }
+    public double getSubTotal() {
         return produto.getPreço() * quantidade;
     }
-
-    Produto getProduto(){
+    public Produto getProduto(){
         return produto;
     }
-    int getQuantidade(){
+    public int getQuantidade(){
         return quantidade;
     }
-
+    
+    private void validarQuantidade(int quantidade){
+        if (quantidade <= 0){
+            throw new IllegalArgumentException("Quantidade invalida");
+        }
+    }
+    private void validarProduto(Produto produto){
+        if (produto == null){
+            throw new IllegalArgumentException("Produto invalido (null)");
+        }
+        if (!produto.isDisponivel()){
+            throw new IllegalArgumentException("Produto indisponivel");
+        }
+    }
 }
